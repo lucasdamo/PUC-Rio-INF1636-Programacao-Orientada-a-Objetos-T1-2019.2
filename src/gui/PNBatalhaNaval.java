d396ea9b3de0b7b706da5e7b6556614405ecf538
Lucas Damo <lucasdamo1@gmail.com>
2019-10-23 14:29:18 -0300

    [fix][feat] Fixed interface and added line/columns labels
    Adicionado indicador do numero nas colunas e linhas, arrumado interface (altura e largura estavam trocados)

--- src/gui/PNBatalhaNaval.java
+++ src/gui/PNBatalhaNaval.java
@@ -33,29 +33,33 @@ public class PNBatalhaNaval extends JPanel implements MouseListener {
 		addMouseListener(this);
 		ctrl = c;
 		for(int i =0; i<15; i++) {
+			// Para cada linha
 			x1 = xIni; x2=xIni2;
 			for(int j=0; j<15; j++) {
+				// Em cada coluna
 				tab1[i][j] = new Celula(x1, y1, larg, alt);
 				tab2[i][j] = new Celula(x2, y2, larg, alt);
-				x1+=alt;
-				x2+=alt;
+				x1+=larg;
+				x2+=larg;
 			}
-			y1+=larg;
-			y2+=larg;
+			y1+=alt;
+			y2+=alt;
 		}
 		y1 = yIni; y2=yIni;
+		// Cria as linhas horizontais
 		for(int i = 0; i<16; i++) {
 			lnY1[i] = new Line2D.Double(xIni,y1,xFim,y1);
 			lnY2[i] = new Line2D.Double(xIni2,y2,xFim2,y2);
-			y1+=larg;
-			y2+=larg;
+			y1+=alt;
+			y2+=alt;
 		}
 		x1 = xIni; x2=xIni2;
+		// Cria as linhas verticais
 		for(int i = 0; i<16; i++) {
 			lnX1[i] = new Line2D.Double(x1,yIni,x1,yFim);
 			lnX2[i] = new Line2D.Double(x2,yIni,x2,yFim);
-			x1+=alt;
-			x2+=alt;
+			x1+=larg;
+			x2+=larg;
 		}
 	}
 	
@@ -70,21 +74,30 @@ public class PNBatalhaNaval extends JPanel implements MouseListener {
 			for(int j=0;j<15;j++) {
 				if(campoJog1[j][i] == 1) g2d.setPaint(Color.red);
 				else g2d.setPaint(Color.cyan);
-				rt=new Rectangle2D.Double(tab1[i][j].x, tab1[i][j].y, tab1[i][j].larg - 4.0, tab1[i][j].alt - 4.0);
+				rt=new Rectangle2D.Double(tab1[i][j].x, tab1[i][j].y, tab1[i][j].alt, tab1[i][j].larg);
 				//System.out.print("Rectangle2D.Double("+tab1[i][j].x+", "+tab1[i][j].y + ", "+tab1[i][j].larg + ", " + tab1[i][j].alt +");\n");
 				g2d.fill(rt);
 				if(campoJog2[j][i] == 1) g2d.setPaint(Color.red);
 				else g2d.setPaint(Color.cyan);
-				rt=new Rectangle2D.Double(tab2[i][j].x, tab2[i][j].y, tab2[i][j].larg - 4.0, tab2[i][j].alt - 4.0);
+				rt=new Rectangle2D.Double(tab2[i][j].x, tab2[i][j].y, tab2[i][j].larg, tab2[i][j].larg);
 				g2d.fill(rt);
 			}
 		}
 		g2d.setPaint(Color.black);
+		// Desenha todas as linhas 
 		for(int i =0; i<16; i++) {
 			g2d.draw(lnX1[i]);
 			g2d.draw(lnX2[i]);
 			g2d.draw(lnY1[i]);
 			g2d.draw(lnY2[i]);
+		}
+		// Desenha os indices
+		for(int i =0; i<15; i++) {
+			String write = Integer.toString(i+1);
+			g2d.drawString(write, (float) ( tab1[0][i].x + larg/2 ), (float) ( yIni - alt/2 ) );
+			g2d.drawString(write, (float) ( xIni - larg/2 ), (float) (tab1[i][0].y + alt/2));
+			g2d.drawString(write, (float) ( tab2[0][i].x + larg/2 ), (float) ( yIni2 - alt/2 ) );
+			g2d.drawString(write, (float) ( xIni2 - larg/2 ), (float) (tab2[i][0].y + alt/2));
 		}
 		
 	}
