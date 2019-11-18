b25914a4bee9801650bcdda8b03e9802203c091d
Lucas Damo <lucasdamo1@gmail.com>
2019-11-18 14:03:11 -0300

    [feat] Criar as classes das armas Criar as classes das armas #1

---
+++ src/armas/Arma.java
@@ -0,0 +1,24 @@
+package armas;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.geom.Line2D.Double;
+import java.awt.event.*;
+import java.util.*;
+import java.util.List;
+import controladores.EstadoJogo;
+
+public abstract class Arma extends JPanel {
+	private Rotacao rot = Rotacao.ZeroGraus;
+	public void rotate() {
+		System.out.print("NEXT ESTADO\n");
+		int index = rot.ordinal();
+		int nextIndex = index + 1;
+		Rotacao[] orientacoes = Rotacao.values();
+		nextIndex %= orientacoes.length;
+		rot = orientacoes[nextIndex];
+	}
+	public abstract void posiciona(int x, int y);
+	public abstract void desenha();
+}
