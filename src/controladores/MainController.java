9bd5d48a63df54e2c4c0c505f26d16ed7deceffb
Lucas Damo <lucasdamo1@gmail.com>
2019-11-19 14:52:15 -0300

    [refactor] Modificar quadradoArma para ter dimensões como variaveis estaticas Tornar as dimensões do quadradoArma estáticas, faz mais sentido pois todos os quadrados terão as mesmas dimensoes e é importante que outros metodos consultem esse tamanho

--- src/controladores/MainController.java
+++ src/controladores/MainController.java
@@ -14,7 +14,7 @@ public class MainController implements Observer {
 	static MainController mainControl = null;
 	public MainController() {
 		MainController.setControl(this);
-		this.estadoAtual = estadoAtual.Batalha;
+		this.estadoAtual = estadoAtual.PosicionamentoArmasJog1;
 		executaEstadoAtual();
 	}
 	
